fc19e58532edc4d17b3e0f8377c5a56cf7d6f355
魏文昌 <wwchang9@126.com>
2019-10-11 15:14:54 +0800

    no message

--- tp/src/HelloWord.java
+++ tp/src/HelloWord.java
@@ -2,7 +2,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("跳舞");
-        System.out.println("玩游戏");
 
         System.out.println("唱歌");
     }
