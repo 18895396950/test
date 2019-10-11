2e88fe01699ad52f5c3c1cf1db50b9b7c4d65d46
魏文昌 <wwchang9@126.com>
2019-10-11 15:20:41 +0800

    加个武功

--- tp/src/HelloWord.java
+++ tp/src/HelloWord.java
@@ -2,7 +2,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("跳舞");
-
+        System.out.println("武功");
         System.out.println("唱歌");
     }
 
