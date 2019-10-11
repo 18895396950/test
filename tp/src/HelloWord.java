ffa8c6af861c8c7ab335e7bc63f00a2bc796be98
魏文昌 <wwchang9@126.com>
2019-10-11 15:08:09 +0800

    加个游戏

--- tp/src/HelloWord.java
+++ tp/src/HelloWord.java
@@ -2,6 +2,7 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("跳舞");
+        System.out.println("玩游戏");
 
         System.out.println("唱歌");
     }
