6c6be699fe0940a0230186163b9b734fee86409f
魏文昌 <wwchang9@126.com>
2019-10-12 08:51:28 +0800

    删减

--- tp/src/HelloWord.java
+++ tp/src/HelloWord.java
@@ -1,15 +1,15 @@
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
-<<<<<<< HEAD
+
         System.out.println("跳舞");
         System.out.println("武功");
         System.out.println("唱歌");
-=======
 
-        System.out.println("唱歌");
-        System.out.println("跳舞");
->>>>>>> branch1
+
+        System.out.println("大人");
+        System.out.println("哈皮");
+
     }
 
 }
